91ef651fdc362f0d6208ccfc904f74626c50e0f3
Student <student@d17-l427-pc-19.ipa.ki.agh.edu.pl>
2019-12-16 09:26:58 +0100

    Kalkulator

--- test.php
+++ test.php
@@ -1,2 +1,24 @@
 <?php
-print "Hello world";
+$znak = $_GET["znak"];
+$liczba1 = $_GET["liczba1"];
+$liczba2 = $_GET["liczba2"];
+if($znak=="+"){
+    $wynik = $liczba1 + $liczba2;
+    print"<h1>$liczba1 $znak $liczba2 = $wynik</h1>";
+}
+else if($znak=="-"){
+    $wynik = $liczba1 - $liczba2;
+    print"<h1>$liczba1 $znak $liczba2 = $wynik</h1>";
+}
+else if($znak=="*"){
+    $wynik = $liczba1 * $liczba2;
+    print"<h1>$liczba1 $znak $liczba2 = $wynik</h1>";
+}
+else if($znak=="/"){
+    $wynik = $liczba1 / $liczba2;
+    print"<h1>$liczba1 $znak $liczba2 = $wynik</h1>";
+}
+else
+    print("Podano bledny znak");
+
+
